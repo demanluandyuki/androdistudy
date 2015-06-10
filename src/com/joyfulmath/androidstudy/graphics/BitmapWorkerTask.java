25ab04d44acfb33653b11cf29512cf1d14eb1ede
luyc1 <luyc1@lenovo.com>
2015-06-10 16:16:08 +0800

    modify async

--- src/com/joyfulmath/androidstudy/graphics/BitmapWorkerTask.java
+++ src/com/joyfulmath/androidstudy/graphics/BitmapWorkerTask.java
@@ -1,13 +1,14 @@
 package com.joyfulmath.androidstudy.graphics;
 
 import java.lang.ref.WeakReference;
+import java.security.PublicKey;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.widget.ImageView;
 
-public class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
+public class BitmapWorkerTask extends AsyncTask<Integer, Integer, Bitmap> {
 
 	private final WeakReference<ImageView> imageViewReference;
     private int data = 0;
@@ -20,13 +21,17 @@ public class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
     }
 
     // Decode image in background.
+    //work thread
     @Override
     protected Bitmap doInBackground(Integer... params) {
+    	publishProgress(0);
         data = params[0];
+        publishProgress(1);
         return GraphicsUtils.decodeSampledBitmapFromResource(mContext.getResources(), data, 100, 100);
     }
 
     // Once complete, see if ImageView is still around and set bitmap.
+    //UI thread
     @Override
     protected void onPostExecute(Bitmap bitmap) {
         if (imageViewReference != null && bitmap != null) {
@@ -35,6 +40,21 @@ public class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
                 imageView.setImageBitmap(bitmap);
             }
         }
+        publishProgress(100);
     }
 
+    //UI thread
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+	}
+
+	//UI thread
+	@Override
+	protected void onProgressUpdate(Integer... values) {
+		super.onProgressUpdate(values);
+	}
+    
+    
 }
+
